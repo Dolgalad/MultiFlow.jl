5d6ade451842725ca4d29519548af0ea1f64c528
Documenter.jl <documenter@juliadocs.github.io>
2024-02-01 10:33:52 +0000

    build based on f09b422

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#MultiFlows.jl-Documentation","page":"MultiFlows.jl Documentation","title":"MultiFlows.jl Documentation","text":"","category":"section"},{"location":"index.html","page":"MultiFlows.jl Documentation","title":"MultiFlows.jl Documentation","text":"func(x)","category":"page"},{"location":"index.html#MultiFlows.func-Tuple{Any}","page":"MultiFlows.jl Documentation","title":"MultiFlows.func","text":"func(x)\n\nReturn double the number x plus 1.\n\n\n\n\n\n","category":"method"}]
+}
