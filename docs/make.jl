3da53d3810aa83b7ae2d8444c68fd729162723a3
Alexandre Schulz <schulz@lipn.univ-paris13.fr>
2024-02-01 11:14:45 +0100

    deplydocs

--- docs/make.jl
+++ docs/make.jl
@@ -2,3 +2,7 @@ using Documenter, MultiFlows
 
 #makedocs(sitename="MultiFlows.jl documentation")
 makedocs(sitename="MultiFlows.jl documentation", format = Documenter.HTML(prettyurls = false))
+
+deploydocs(
+           repo = "github.com/Dolgalad/MultiFlows.jl.git",
+          )
