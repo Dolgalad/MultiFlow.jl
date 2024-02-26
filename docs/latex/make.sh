a7cc923f227953bc6956a7a4b7d31f4919a6e096
Alexandre Schulz <schulz@lipn.univ-paris13.fr>
2024-02-26 11:15:46 +0100

    make tex

--- docs/latex/make.sh
+++ docs/latex/make.sh
@@ -7,7 +7,7 @@ echo $(pwd)
 # Make latex tikz images
 for FILE in ./img/*.tex; do
 	echo $FILE
-	pdflatex -output-directory `dirname $FILE` $FILE > /dev/null 2>&1
+	pdflatex -output-directory `dirname $FILE` $FILE #> /dev/null 2>&1
 	ls img
 	convert -density 600 "${FILE%.tex}.pdf" "${FILE%.tex}.png" > /dev/null 2>&1
 	mv "${FILE%.tex}.png" ../src/assets/img
