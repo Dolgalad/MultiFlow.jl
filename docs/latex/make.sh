91189a28050c53423ad6cc3eb232cdc0feda3adf
Alexandre Schulz <schulz@lipn.univ-paris13.fr>
2024-02-26 11:51:36 +0100

    make tex

--- docs/latex/make.sh
+++ docs/latex/make.sh
@@ -13,8 +13,9 @@ for FILE in ./img/*.tex; do
 		echo "PDF output $PDFFILE does not exist. Quitting"
 		exit 1
 	fi
-	convert -density 600 "${FILE%.tex}.pdf" "${FILE%.tex}.png" #> /dev/null 2>&1
+	#convert -density 600 "${FILE%.tex}.pdf" "${FILE%.tex}.png" #> /dev/null 2>&1
 	PNGFILE="${FILE%.tex}.png"
+	gs -dSAFER -r600 -sDEVICE=pngalpha -o "$PNGFILE" "$PDFFILE"
 	if [ ! -f "$PNGFILE" ]; then
 		echo "PNG output $PNGFILE does not exist. Quitting"
 		exit 1
